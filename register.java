28890575bf90112f9d66743337a1c06e57bb092c
BurakuslenderaTR <72018039+BurakuslenderaTR@users.noreply.github.com>
2020-11-27 23:49:06 +0300

    Update register.java

--- register.java
+++ register.java
@@ -12,15 +12,15 @@ public class register {
         pasword=scn.nextLine();
         if (kişi_adi.equals("Burak") && pasword.equals("123")) {
             System.out.println("your login is successful");
-        }else if(user.equals("hamza") && pasword.equals("1234")){
+        }else if(user.equals("Manyak") && pasword.equals("228")){
             System.out.println("your login is successful");
         }else if(user.equals("Meral") && pasword.equals("12345")) {
             System.out.println("your login is successful");
         }else if(user.equals("tuce") && pasword.equals("123456")) {
             System.out.println("your login is successful");
-        }else if (user.equals("burak") && pasword.equals( "1234567")) {
+        }else if (user.equals("Mustafa") && pasword.equals( "2208")) {
             System.out.println("your login is successful");
-        }else if(user.equals("nevin") && pasword.equals("1234567")) {
+        }else if(user.equals("Murat") && pasword.equals("1234567")) {
             System.out.println("your login is successful");
         }else if(user.equals("aslan") && paswaord.equals("12346578")) {
             System.out.println("your login is successful");
